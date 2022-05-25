fe1ae314b26cff1d435148a9fe6ed7dd98ac488a
lachryn <kimjh5@humaxparcs.com>
2022-05-25 13:59:13 +0900

    add image-temp for lpr

--- SimpleFileServer.py
+++ SimpleFileServer.py
@@ -10,6 +10,11 @@ from fastapi_utils.tasks import repeat_every
 
 logger = logging.getLogger(__name__)
 data_dir = "/data/image.server"
+temp_dir = data_dir + "/temp"
+
+if not os.path.isdir(temp_dir):
+    os.makedirs(temp_dir)
+
 keep_file_days = 15
 
 app = FastAPI()
@@ -23,6 +28,17 @@ def periodic_cleaner():
         fname = os.path.join(data_dir, f)
         if os.stat(fname).st_mtime < now:
             os.remove(fname)
+
+
+@app.post("/v1/image-temp")
+async def upload_file(file: UploadFile = File(...)):
+    save_file_name = os.path.basename(file.filename)
+    data_name = os.path.join(temp_dir, save_file_name)
+    with open(data_name, 'wb') as image:
+        content = await file.read()
+        image.write(content)
+        image.close()
+    return JSONResponse(content={"uuid": save_file_name}, status_code=200)
 
 
 @app.post("/v1/upload")
@@ -45,4 +61,4 @@ if __name__ == "__main__":
     if not os.path.isdir(data_dir):
         os.makedirs(data_dir)
 
-    uvicorn.run(app, port=8001, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
