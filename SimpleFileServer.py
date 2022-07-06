eb1d7d7284fa44c3c6eb6abec26c78c490c2aefd
doroami <kimjh5@humaxparcs.com>
2022-07-06 21:52:07 +0900

    #3 add filter and download function

--- SimpleFileServer.py
+++ SimpleFileServer.py
@@ -1,15 +1,15 @@
 import datetime
 import glob
+import io
 import logging
 import os
 import shutil
-import time
-import uuid
 
 import uvicorn
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse
 from fastapi_utils.tasks import repeat_every
+from starlette.responses import StreamingResponse
 
 logger = logging.getLogger(__name__)
 data_dir = "/data/APGS/images"
@@ -62,6 +62,34 @@ async def upload_file_temp(file: UploadFile = File(...)):
     return JSONResponse(content={"uuid": save_file_name}, status_code=200)
 
 
+@app.get("/v1/download/latest")
+async def download_latest(lpr: str):
+    if lpr in stored_data_dict:
+        data_name = stored_data_dict[lpr][-1]
+        return StreamingResponse(io.BytesIO(open(data_name, "rb").read()), media_type="image/jpg")
+        # return FileResponse(path=data_name, media_type="image/jpeg", filename=os.path.basename(data_name))
+    return JSONResponse(content="error", status_code=404)
+
+
+@app.get("/v1/search")
+async def search(lpr: str):
+    found_list = []
+    start = len(data_dir)
+    for _, data in stored_data_dict.items():
+        if lpr in _:
+            found_list += [d[start + 1:] for d in data]
+
+    return JSONResponse(content=found_list, status_code=200)
+
+
+@app.get("/v1/download")
+async def download_specific(path_name: str):
+    full_name = os.path.join(data_dir, path_name)
+    if os.path.isfile(full_name):
+        return StreamingResponse(io.BytesIO(open(full_name, "rb").read()), media_type="image/jpg")
+
+    return JSONResponse(content="error", status_code=404)
+
 # @app.post("/v1/upload")
 # async def upload_file(file: UploadFile = File(...)):
 #     save_file_name = str(uuid.uuid1()) + ".jpg"
@@ -76,11 +104,6 @@ async def upload_file_temp(file: UploadFile = File(...)):
 # @app.get("/v1/download/{name_file}")
 # def download_file(name_file: str):
 #     return FileResponse(path=os.path.join(data_dir, name_file), media_type='application/octet-stream', filename=name_file)
-
-
-@app.get("/v1/download_temp/{name_file}")
-def download_file_temp(name_file: str):
-    return FileResponse(path=os.path.join(temp_dir, name_file), media_type='image/jpeg', filename=name_file)
 
 
 if __name__ == "__main__":
